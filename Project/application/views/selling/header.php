2f24be523e5ef9a1dd1df14bf0152f34634da1a0
bamke25 <nurullahke25@gmail.com>
2020-11-29 17:26:11 +0700

    8.2

--- Project/application/views/selling/header.php
+++ Project/application/views/selling/header.php
@@ -93,14 +93,6 @@
                                <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                                My Profile
                            </a>
-                           <!-- <a class="dropdown-item" href="#">
-                                    <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Settings
-                                </a>
-                                <a class="dropdown-item" href="#">
-                                    <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Activity Log
-                                </a> -->
                            <div class="dropdown-divider"></div>
                            <a class="dropdown-item" href="<?= base_url('auth/logout'); ?>" data-toggle="modal" data-target="#logoutModal">
                                <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
@@ -108,9 +100,6 @@
                            </a>
                        </div>
                    </li>
-
-
-
               </ul>
             </nav>
           </div>
