ffd48d5253f6ae7b611ed74514603709c585c115
bamke25 <nurullahke25@gmail.com>
2020-11-30 17:01:13 +0700

    9

---
+++ Project/application/controllers/Menu.php
@@ -0,0 +1,36 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Menu extends CI_Controller
+{
+    public function index()
+    {
+        $data['title'] = 'Menu Management';
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+
+        $data['menu'] = $this->db->get('user_menu')->result_array();
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('menu/index', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function addmenu()
+    {
+        $data['title'] = 'Menu Management';
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        $data['menu'] = $this->db->get('user_menu')->result_array();
+
+        $this->form_validation->set_rules('menu', 'Menu', 'required');
+        if ($this->form_validation->run()== false)
+            {
+                $this->load->view('templates/header', $data);
+                $this->load->view('templates/sidebar', $data);
+                $this->load->view('templates/topbar', $data);
+                $this->load->view('menu/addmenu', $data);
+                $this->load->view('templates/footer');
+            }
+    }
+}
--- Project/application/views/templates/sidebar.php
+++ Project/application/views/templates/sidebar.php
@@ -14,36 +14,53 @@
         <!-- <hr class="sidebar-divider my-0"> -->
         <hr class="sidebar-divider">
 
+        <!-- QUERY MENU -->
+        <?php 
+        $role_id = $this->session->userdata('role_id');
+        $queryMenu = "SELECT user_menu.id, menu
+                        FROM user_menu JOIN user_access_menu 
+                          ON user_menu.id = user_access_menu.menu_id
+                       WHERE user_access_menu.role_id = $role_id
+                       ORDER BY user_access_menu.menu_id ASC
+                    ";
+        $menu = $this->db->query($queryMenu)->result_array(); 
+        ?>
+
+        <!-- LOOPING MENU -->
+        <?php foreach($menu as $m) : ?>
+        <div class="sidebar-heading">
+            <?= $m['menu']; ?>
+        </div>
         
+        <!-- SIAPKAN SUB-MENU SESUAI MENU -->
+        <?php 
+        $menuId = $m['id'];
+        $querySubMenu = "SELECT *
+                        FROM user_sub_menu JOIN user_menu 
+                          ON user_sub_menu.menu_id = user_menu.id
+                       WHERE user_sub_menu.menu_id = $menuId
+                         AND user_sub_menu.is_active = 1
+                    ";
+        $subMenu = $this->db->query($querySubMenu)->result_array();
+        ?>
 
-        <!-- Heading -->
-        <div class="sidebar-heading">
-            Administrator
-        </div>
-
-        <!-- Nav Item - Dashboard -->
-        <li class="nav-item">
-            <a class="nav-link" href="index.html">
-                <i class="fas fa-fw fa-tachometer-alt"></i>
-                <span>Dashboard</span></a>
+        <?php foreach($subMenu as $sm): ?>
+            <?php if ($title == $sm['title']) : ?>
+            <li class="nav-item active">
+                <?php else : ?>
+            <li class="nav-item">
+                <?php endif; ?>
+            <a class="nav-link" href="<?= base_url($sm['url']); ?>">
+                <i class="<?= $sm['icon']; ?>"></i>
+                <span><?= $sm['title']; ?></span></a>
         </li>
+        <?php endforeach; ?>
 
-        <!-- Divider -->
+            <!-- Divider -->
         <hr class="sidebar-divider">
 
-        <!-- Heading -->
-        <div class="sidebar-heading">
-            User
-        </div>
+        <?php endforeach; ?>
 
-        <li class="nav-item">
-            <a class="nav-link" href="charts.html">
-                <i class="fas fa-fw fa-user"></i>
-                <span>My Profile</span></a>
-        </li>
-
-        <!-- Divider -->
-        <hr class="sidebar-divider">
 
         <li class="nav-item">
             <a class="nav-link" href="<?= base_url('auth/logout') ?>">
