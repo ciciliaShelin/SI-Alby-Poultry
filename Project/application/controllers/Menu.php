2298e2167b120c2234232dd69e748c2f97a6faee
bamke25 <nurullahke25@gmail.com>
2020-12-01 07:17:57 +0700

    9

--- Project/application/controllers/Menu.php
+++ Project/application/controllers/Menu.php
@@ -7,30 +7,41 @@ class Menu extends CI_Controller
     {
         $data['title'] = 'Menu Management';
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
-
-        $data['menu'] = $this->db->get('user_menu')->result_array();
-
-        $this->load->view('templates/header', $data);
-        $this->load->view('templates/sidebar', $data);
-        $this->load->view('templates/topbar', $data);
-        $this->load->view('menu/index', $data);
-        $this->load->view('templates/footer');
-    }
-
-    public function addmenu()
-    {
-        $data['title'] = 'Menu Management';
-        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
         $data['menu'] = $this->db->get('user_menu')->result_array();
 
         $this->form_validation->set_rules('menu', 'Menu', 'required');
         if ($this->form_validation->run()== false)
+        {
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/sidebar', $data);
+            $this->load->view('templates/topbar', $data);
+            $this->load->view('menu/index', $data);
+            $this->load->view('templates/footer');
+        } else 
             {
-                $this->load->view('templates/header', $data);
-                $this->load->view('templates/sidebar', $data);
-                $this->load->view('templates/topbar', $data);
-                $this->load->view('menu/addmenu', $data);
-                $this->load->view('templates/footer');
+                $this->db->insert('user_menu', ['menu'=> $this->input->post('menu')]);
+                $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+                New Menu Added!</div>');
+                redirect('menu');
             }
+
     }
+
+    public function submenu()
+    {
+        $data['title'] = 'Submenu Management';
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        $data['menu'] = $this->db->get('user_menu')->result_array();
+
+        $this->load->model('Menu_model', 'menu');
+        $data['subMenu'] = $this->db->menu->getSubMenu();
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('menu/submenu', $data);
+        $this->load->view('templates/footer');
+    }
+
+   
 }
