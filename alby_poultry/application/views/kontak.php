909ace673b8602446d893a1f9355e3ab63f98aa3
eva agustin <evaagustin766@gmail.com>
2020-12-09 05:03:21 +0700

    kontak

---
+++ alby_poultry/application/views/kontak.php
@@ -0,0 +1,77 @@
+<div class="container-fluid">
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="<?php echo base_url('assets/img/slide3.jpg') ?>" width='400px' height='500px' class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="<?php echo base_url('assets/img/slide 8.jpg') ?>" width='400px' height='500px' class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</br>
+</br>
+</br>
+</br>
+<section class="ftco-section contact-section ftco-no-pb" id="contact-section">
+      <div class="container align-items-center justify-content-center">
+      	
+
+        <div class="row d-flex contact-info mb-5">
+          <div class="col-md-6 col-lg-3 d-flex ftco-animate">
+          	<div class="align-self-stretch box p-4 text-center">
+          		<div class="icon d-flex align-items-center justify-content-center">
+          			<span class="icon-map-signs"></span>
+          		</div>
+          		<h3 class="mb-4">Alamat</h3>
+              <p>
+              Dsn.Ngepeh, Ds.Rejoagung, Kec.Ngoro, Kab. Jombang, Jawa Timu</p>
+              <a target="_blank" rel="nofollow"  href="https://g.page/Macarina?share" class="smoothscroll btn btn-primary">Lihat di Gmaps</a></p>
+	            <p>
+	          </div>
+          </div>
+          <div class="col-md-6 col-lg-3 d-flex ftco-animate">
+          	<div class="align-self-stretch box p-4 text-center">
+          		<div class="icon d-flex align-items-center justify-content-center">
+          			<span class="icon-phone2"></span>
+          		</div>
+          		<h3 class="mb-4">Kontak Langsung</h3>
+	            <p><a >+6285866195648</a></p>
+              <br></br>
+              <a target="_blank" rel="nofollow"  href="https://api.whatsapp.com/send?phone=6285866195648&text=Kak,%20Saya%20dari%20website%20,mau%20pesan%20Macarina:" class="smoothscroll btn btn-primary">Pesan via Whatsapp</a></p>
+	            <p>
+	          </div>
+          </div>
+          <div class="col-md-6 col-lg-3 d-flex ftco-animate">
+          	<div class="align-self-stretch box p-4 text-center">
+          		<div class="icon d-flex align-items-center justify-content-center">
+          			<span class="icon-paper-plane"></span>
+          		</div>
+          		<h3 class="mb-4">Alamat Email</h3>
+	            <p><a href="mailto:macarina.official@gmail.com">albypoultry@gmail.com</a></p>
+	          </div>
+          </div>
+          <div class="col-md-6 col-lg-3 d-flex ftco-animate">
+          	<div class="align-self-stretch box p-4 text-center">
+          		<div class="icon d-flex align-items-center justify-content-center">
+          			<span class="icon-globe"></span>
+          		</div>
+          		<h3 class="mb-4">Website</h3>
+	            <p><a href="#">albypoultry.com</a></p>
+	          </div>
+          </div>
+        </div>
+</div>
