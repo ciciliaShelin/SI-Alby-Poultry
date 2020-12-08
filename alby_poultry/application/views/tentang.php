d1b53405e6c045c3cb9ec918f92c6bf85406dac8
eva agustin <evaagustin766@gmail.com>
2020-12-09 05:03:46 +0700

    create tentang

---
+++ alby_poultry/application/views/tentang.php
@@ -0,0 +1,119 @@
+<div class="container-fluid">
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="<?php echo base_url('assets/img/slide3.jpg') ?>" width='400px' height='500px' class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="<?php echo base_url('assets/img/slide 8.jpg') ?>" width='400px' height='500px' class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+<div class="col-md-7 py-5 wrap-about pb-md-5 ftco-animate">
+	          <div class="heading-section-bold mb-4 mt-md-5">
+            <div class="ml-md-0">
+		            <h2 class="mb-4">Alby Poultry</h2>
+	            </div>
+	          </div>
+	          <div class="pb-md-5">
+							<p> Alby Poultry merupakan toko yang berdiri sejak tahun 2012.Toko ini bergerak dalam bidang usaha Supplier Sapronak seperti : Tempat Minum ayam otomatis (TMAO), Tempat Pakan Ayam (TPA), Baby Chick Feeder (BCF) untuk anak ayam, Pemanas Anak Ayam/Gas Brooder, NIPPLE, dll. Melalui situs ini, Anda akan mendapatkan produk peralatan kandang langsung dari produsennya.Karna langsung dari produsennya otomatis Anda akan mendapatkan barang yang lebih murah dari pasaran.  Dengan harga murah dan terjangkau, dengan sistem terima barang pesanan di alamat masing-masing.</p><br/>
+						
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+    <div class="container">
+    <div class="row mb-5">
+          <div class="col-md-8">
+            <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">Visi dan Misi</h2>
+              <p>Visi : </p>
+              <p>
+              1. Bermanfaat untuk banyak orang </br>
+              
+              2. Memiliki Usaha Alat Ternak yang terbesar di Indonesia </br>
+              
+              3. Menjadi penyedia alat-alat ternak ayam yang berkualitas </br>
+              </p>
+
+              <p>Misi : <p>
+              <p>
+              1. Menambah pelanggan dengan edukasi pasar yang berkesinambungan </br>
+
+              2. Terus berinovasi menciptakan produk yang aman dan layak untuk dikonsumsi </br>
+
+              3. Menjaga kualitas produksi dengan bahan baku terbaik </br>
+              </p>
+                                        
+              
+            </div>
+          </div>
+          </div>
+         
+          <div class="row mb-5">
+          <div class="col-md-8">
+            <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">Data Usaha</h2>
+              <table >
+              <tr>
+              <td width="150px"><h5>Nama Usaha</h5></td>
+              <td width ="10px"><h5>:</h5></td>
+              <td width="500px"><h5>Alby Poultry</h5></td>
+              </tr>
+              <tr>
+              <td width="150px"><h5>Owner</h5></td>
+              <td width ="10px"><h5>:</h5></td>
+              <td width="500px"><h5>Andik Siswanto</h5></td>
+              </tr>
+              <tr>
+              <td width="150px"><h5>Bidang Usaha</h5></td>
+              <td width ="10px"><h5>:</h5></td>
+              <td width="500px"><h5>Alat Ternak Ayam</h5></td>
+              </tr>
+              <tr>
+              <td width="150px"><h5>Jenis Produk</h5></td>
+              <td width ="10px"><h5>:</h5></td>
+              <td width="500px"><h5>Alat Ternak</h5></td>
+              </tr>
+              <tr>
+              <td width="150px"><h5>Alamat</h5></td>
+              <td width ="10px"><h5>:</h5></td>
+              <td width="500px"><h5>Dsn.Ngepeh, Ds.Rejoagung, Kec.Ngoro, Kab. Jombang, Jawa Timur</h5></td>
+              </tr>
+              <tr>
+              <td width="150px"><h5>Nomor Telp</h5></td>
+              <td width ="10px"><h5>:</h5></td>
+              <td width="500px"><h5>+6285866195648</h5></td>
+              </tr>
+              <tr>
+              <td width="150px"><h5>Alamat Email</h5></td>
+              <td width ="10px"><h5>:</h5></td>
+              <td width="500px"><h5>albypoultry@gmail.com</h5></td>
+              </tr>
+              </table>
+              </div>
+              </div>
+              </div>
+              <div class="row mb-5">
+          <div class="col-md-8">
+            <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">LEGALITAS USAHA</h2>
+          </div>
+</div>
+</div>
