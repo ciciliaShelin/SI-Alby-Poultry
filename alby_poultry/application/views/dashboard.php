37a888308eb4365b130ba3fb43640acef68dcb60
eva agustin <evaagustin766@gmail.com>
2020-12-15 16:15:15 +0700

    update dashboard

--- alby_poultry/application/views/dashboard.php
+++ alby_poultry/application/views/dashboard.php
@@ -44,7 +44,7 @@
                 <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
                     <div class="wow fadeIn" data-wow-duration="1s" data-wow-delay="0.1s">
                         <h2 class="block-title" align="center"> What is Alby Poultry? </h2>
-                        <p align="justify"> Alby Poultry merupakan toko yang berdiri sejak tahun 2017.Toko ini bergerak dalam bidang usaha Supplier Sapronak seperti : Tempat Minum ayam otomatis (TMAO), Tempat Pakan Ayam (TPA), Baby Chick Feeder (BCF) untuk anak ayam, Pemanas Anak Ayam/Gas Brooder, NIPPLE, dll. Melalui situs ini, Anda akan mendapatkan produk peralatan kandang langsung dari produsennya.Karna langsung dari produsennya otomatis Anda akan mendapatkan barang yang lebih murah dari pasaran.  Dengan harga murah dan terjangkau, dengan sistem terima barang pesanan di alamat masing-masing.</p>
+                        <p align="justify"> Alby Poultry merupakan toko yang berdiri sejak tahun 2012.Toko ini bergerak dalam bidang usaha Supplier Sapronak seperti : Tempat Minum ayam otomatis (TMAO), Tempat Pakan Ayam (TPA), Baby Chick Feeder (BCF) untuk anak ayam, Pemanas Anak Ayam/Gas Brooder, NIPPLE, dll. Melalui situs ini, Anda akan mendapatkan produk peralatan kandang langsung dari produsennya.Karna langsung dari produsennya otomatis Anda akan mendapatkan barang yang lebih murah dari pasaran.  Dengan harga murah dan terjangkau, dengan sistem terima barang pesanan di alamat masing-masing.</p>
                         <p > Buat kamu yang ingin mencari barang barang ternak, <h6 class="block-title"> DISINILAH SOLUSINYA!!!! </h6> </p>
                         <p ><a href="dashboard/shop" class="smoothscroll btn btn-primary">Beli Sekarang</a></p>
                     </div>
@@ -62,8 +62,9 @@
     </div> 
     <br>
     <br>
-        <iframe width="420" height="315"src="https://youtube.com/embed/BbS2kUYp7TI"></iframe> 
-                <div class="embed-responsive embed-responsive-16by9" style="margin-bottom:20px">
+    
+            <div style="padding:15px 50px;line-height:19px;min-height:65px">
+            <img src="<?php echo base_url('uploads/alby.png') ?>"width='420px' height='315px'  alt="Image" class="img-fluid">
     </div>
     </div>
     </div> 
@@ -78,7 +79,7 @@
     </div> 
             <div class="the-card" style="background:rgba(255,255,255,.85);margin-bottom:30px">
             <div style="padding-top:5px;padding-bottom:5px;max-width:95%;width:94%;border-radius:5px;box-shadow:1px 1px 4px #666;background-color:#0000FF;color:#fff;-webkit-transform:translateZ(0);-moz-transform:translateZ(0);-ms-transform:translateZ(0);-o-transform:translateZ(0);transform:translateZ(0);-webkit-transition:all .5s ease-out;-moz-transition:all .5s ease-out;transition:all .5s ease-out;margin-top:-20px">
-                    <h2 style="line-height:50px;color:#fff"></h2> <a target="_blank" rel="nofollow"  href="https://api.whatsapp.com/send?phone=6285866195648&text=Kak,%20Saya%20dari%20website%20,mau%20pesan%20Macarina:" class="smoothscroll btn btn-primary">Pesan via Whatsapp</a></p>
+                    <h2 style="line-height:50px;color:#fff"></h2> <a target="_blank" rel="nofollow"  href="https://api.whatsapp.com/send?phone=6285866195648&text=Kak,%20Saya%20dari%20website%20,mau%20pesan%20Alat%20Peternakan:" class="smoothscroll btn btn-primary">Pesan via Whatsapp</a></p>
 	            <p>
                    
     </div> 
